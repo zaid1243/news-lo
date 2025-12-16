af8a4df13024b99cc302cfb9df1ed574fda0a4d5
zaid <skzaid8821@email.com>
2025-12-17 01:31:46 +0530

    set base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: "/news-lo/",
   plugins: [react(), tailwindcss()],
 });
